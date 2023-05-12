2985944d586bd757af1c8a2efba11836b256d7d9
ElBe <90863907+ElBe-Plaq@users.noreply.github.com>
2023-05-12 07:24:39 +0200

    Fixed indentation

--- src/Installer/Windows/add_desktop_icon.bat
+++ src/Installer/Windows/add_desktop_icon.bat
@@ -31,11 +31,11 @@ SET Esc_LinkTarget=%%SYSTEMROOT%%\notepad.exe
 SET cSctVBS=CreateShortcut.vbs
 SET LOG=".\%~N0_runtime.log"
 ((
-  echo Set oWS = WScript.CreateObject^("WScript.Shell"^)
-  echo sLinkFile = oWS.ExpandEnvironmentStrings^("!Esc_LinkDest!"^)
-  echo Set oLink = oWS.CreateShortcut^(sLinkFile^)
-  echo oLink.TargetPath = oWS.ExpandEnvironmentStrings^("!Esc_LinkTarget!"^)
-  echo oLink.Save
+	echo Set oWS = WScript.CreateObject^("WScript.Shell"^)
+	echo sLinkFile = oWS.ExpandEnvironmentStrings^("!Esc_LinkDest!"^)
+	echo Set oLink = oWS.CreateShortcut^(sLinkFile^)
+	echo oLink.TargetPath = oWS.ExpandEnvironmentStrings^("!Esc_LinkTarget!"^)
+	echo oLink.Save
 )1>!cSctVBS!
 cscript //nologo .\!cSctVBS!
 DEL !cSctVBS! /f /q
