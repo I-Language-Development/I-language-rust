4109e20d35aca189206e5e65e2d54a117b060abd
ElBe <90863907+ElBe-Plaq@users.noreply.github.com>
2023-05-29 19:35:07 +0200

    Changed comment type

--- src/Installer/Windows/add_desktop_icon.bat
+++ src/Installer/Windows/add_desktop_icon.bat
@@ -1,29 +1,29 @@
-rem I Language windows desktop icon tool.
-rem Version: 1.0.0
+: I Language windows desktop icon tool.
+: Version: 1.0.0
 rem
-rem Copyright (c) 2023-present I Language Development.
+: Copyright (c) 2023-present I Language Development.
 rem
-rem Permission is hereby granted, free of charge, to any person obtaining a
-rem copy of this software and associated documentation files (the 'Software'),
-rem to deal in the Software without restriction, including without limitation
-rem the rights to use, copy, modify, merge, publish, distribute, sublicense,
-rem and/or sell copies of the Software, and to permit persons to whom the
-rem Software is furnished to do so, subject to the following conditions:
+: Permission is hereby granted, free of charge, to any person obtaining a
+: copy of this software and associated documentation files (the 'Software'),
+: to deal in the Software without restriction, including without limitation
+: the rights to use, copy, modify, merge, publish, distribute, sublicense,
+: and/or sell copies of the Software, and to permit persons to whom the
+: Software is furnished to do so, subject to the following conditions:
 rem
-rem The above copyright notice and this permission notice shall be included in
-rem all copies or substantial portions of the Software.
+: The above copyright notice and this permission notice shall be included in
+: all copies or substantial portions of the Software.
 rem
-rem THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS
-rem OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-rem FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-rem AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-rem LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-rem FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-rem DEALINGS IN THE SOFTWARE.
+: THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS
+: OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+: FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+: AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+: LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+: FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+: DEALINGS IN THE SOFTWARE.
 
 @echo off
 
-rem Copied from https://superuser.com/a/392082
+: Copied from https://superuser.com/a/392082
 SETLOCAL ENABLEDELAYEDEXPANSION
 SET LinkName=I Language Shell
 SET Esc_LinkDest=%%HOMEDRIVE%%%%HOMEPATH%%\Desktop\!LinkName!.lnk
--- src/Installer/Windows/associate_file_extension.bat
+++ src/Installer/Windows/associate_file_extension.bat
@@ -1,35 +1,35 @@
-rem I Language windows file extension associate tool.
-rem Version: 1.0.0
+: I Language windows file extension associate tool.
+: Version: 1.0.0
 rem
-rem Copyright (c) 2023-present I Language Development.
+: Copyright (c) 2023-present I Language Development.
 rem
-rem Permission is hereby granted, free of charge, to any person obtaining a
-rem copy of this software and associated documentation files (the 'Software'),
-rem to deal in the Software without restriction, including without limitation
-rem the rights to use, copy, modify, merge, publish, distribute, sublicense,
-rem and/or sell copies of the Software, and to permit persons to whom the
-rem Software is furnished to do so, subject to the following conditions:
+: Permission is hereby granted, free of charge, to any person obtaining a
+: copy of this software and associated documentation files (the 'Software'),
+: to deal in the Software without restriction, including without limitation
+: the rights to use, copy, modify, merge, publish, distribute, sublicense,
+: and/or sell copies of the Software, and to permit persons to whom the
+: Software is furnished to do so, subject to the following conditions:
 rem
-rem The above copyright notice and this permission notice shall be included in
-rem all copies or substantial portions of the Software.
+: The above copyright notice and this permission notice shall be included in
+: all copies or substantial portions of the Software.
 rem
-rem THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS
-rem OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-rem FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-rem AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-rem LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-rem FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-rem DEALINGS IN THE SOFTWARE.
+: THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS
+: OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+: FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+: AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+: LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+: FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+: DEALINGS IN THE SOFTWARE.
 
 @echo off
 
-rem Run as new process with administrator privileges
+: Run as new process with administrator privileges
 net session >nul 2>&1 || (powershell start -verb runas '"%~0"' %1 & exit /b)
 
-rem Global registry path
+: Global registry path
 set registry_path="HKEY_LOCAL_MACHINE\Software\CLASSES"
 
-rem Add file extension to registry
+: Add file extension to registry
 reg add "%registry_path%\ILanguage" /d "I Language File" /f
 reg add "%registry_path%\ILanguage" /v "AppUserModelID" /d "ILanguage.ILanguage" /f
 reg add "%registry_path%\ILanguage\DefaultIcon" /d "%PROGRAMFILES%\ilanguage\icon.ico" /f
