c0adca6dc90c7e9b18a3b218b9d619178438ad2d
ElBe <90863907+ElBe-Plaq@users.noreply.github.com>
2023-09-20 08:51:19 +0200

    Added translations and changed names to lowercase

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 /*
 I Language lib.
-Version: 0.1.0
+Version: 0.1.1
 
 Copyright (c) 2023-present I Language Development.
 
@@ -23,6 +23,40 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-pub mod Compiler;
-pub mod Grammar;
-pub mod Parser;
+/////////////
+// EXPORTS //
+/////////////
+
+pub mod compiler;
+pub mod grammar;
+pub mod parser;
+
+
+/////////////
+// IMPORTS //
+/////////////
+
+use std;
+
+use current_locale;
+use localizer_rs;
+
+
+//////////////////
+// TRANSLATIONS //
+//////////////////
+
+pub fn get_config() -> localizer_rs::Config {
+    let locale: String = match current_locale::current_locale() {
+        Ok(value) => {
+            let return_value: String = value.to_owned();
+            return_value
+        },
+        Err(error) => {
+            eprintln!("{}", error);
+            std::process::exit(1);
+        }
+    };
+    let split_locale: Vec<&str> = locale.split("-").collect();
+    return localizer_rs::Config::new("Translations", split_locale[0]);
+}
