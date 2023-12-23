21c1f5dd7baf1e1a974bdb870846517cc547c701
ElBe <90863907+ElBe-Plaq@users.noreply.github.com>
2023-12-23 12:38:27 +0100

    [gh-91]: Added tokens

---
+++ src/lexer/src/tokens/token.rs
@@ -0,0 +1,89 @@
+// I Language lexer tokens.
+// Version: 1.0.0
+//
+// Copyright (c) 2023-present I Language Development.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the 'Software'),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+/////////////
+// IMPORTS //
+/////////////
+
+use std;
+
+use crate::tokens::constant::{Constant, Type};
+use crate::tokens::keyword::Keyword;
+use crate::tokens::mark::Mark;
+
+
+////////////
+// TRAITS //
+////////////
+
+pub trait GetToken {
+    fn get_token(location: Location, buffer: &Vec<char>) -> Option<(Token, usize)>;
+}
+
+
+////////////////////////////////
+// TYPE DEFINITION TOKEN TYPE //
+////////////////////////////////
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub enum TypeDefinition {
+    Str,
+    Integer,
+    Float,
+    Boolean(Constant),
+    None
+}
+
+
+//////////////////
+// TOKEN STRUCT //
+//////////////////
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub enum TokenType {
+    Type(Type),
+    TypeDefinition(TypeDefinition),
+    Keyword(Keyword),
+    Mark(Mark),
+    Identifier
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Location {
+    pub file: String,
+    pub line: usize,
+    pub column: usize
+}
+
+impl std::fmt::Display for Location {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(formatter, "{}:{}:{}", self.file, self.line, self.column)
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Token {
+    pub location: Location,
+    pub content: String,
+    pub token_type: TokenType
+}
